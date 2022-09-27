2a2127bcf068aa18dc0edf6668d61ff975bf5103
William Xavier <willian.xavier@alunos.sc.senac.br>
2022-09-26 21:05:25 -0300

    feita conexão com banco de dados

---
+++ src/br/senac/conexaoBD/Conexao.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.conexaoBD;
+
+import java.sql.*;
+
+/**
+ *
+ * @author willian.xavier
+ */
+public class Conexao {
+
+    public static Connection Conector() {
+        Connection conexao = null;
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/senac";
+        String user = "root";
+        String password = "123qwe@";
+
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception elefante) {
+            System.out.println(elefante);
+            return null;
+        }
+
+    }
+}
